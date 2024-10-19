b2a95fb46fa27377d3899f61af754f6e2d86ad04
dejtes <dejenaref@gmail.com>
2024-10-19 08:29:07 -0400

    get user by id implemented

--- furniture_store/DAO/user_dao.py
+++ furniture_store/DAO/user_dao.py
@@ -1,3 +1,20 @@
 from mysql.connector import connect, Error
 from db.connection import get_db_connection
- 
+
+class UserDAO:
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+            return cursor.fetchone()
+        except Error as e:
+            print("Error fetching user by id:", e)
+            return None
+        finally:
+            cursor.close()
+            connection.close()
+    
+
